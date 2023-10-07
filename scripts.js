a5413ec6b60cd9ed2a16b8dc1d92ca8388480692
Hokann <hokan4316v3@gmail.com>
2023-10-07 13:44:24 -0400

    finish basic 1 color E-a-S. Add comments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//creates grid of size size, and returns an array of the divs created
 function createGrid(size){
     gridArr = [];
     const gridBox = document.querySelector('.grid-box');
@@ -20,6 +21,7 @@ function createGrid(size){
     return gridArr;
 }
 
+//deletes grid row by row
 function removeGrid(size){
     for (i=0; i<size; i++){
         let rowToDelete = document.querySelector('.row'+i)
@@ -29,17 +31,20 @@ function removeGrid(size){
 
 //init
 let size = 16;
+//array of the individual divs of the grid, -> gridElements
 let gridElements = createGrid(size);
 
+//when mouse is hovering inside the gridbox. Take each div from array and change its style
 const insideGridBox = document.querySelector('.grid-box');
 insideGridBox.addEventListener('mouseenter', function(){
     gridElements.forEach(element => {
         element.addEventListener('mouseover', function(){
-            element.style.backgroundColor = 'blue';
+            element.style.backgroundColor = 'black';
         }); 
     });
 });
 
+//when change size button is pressed, remove grid and create grid of new size & new array.
 const gridSizeBtn = document.querySelector('#size');
 gridSizeBtn.addEventListener('click', function(){
     removeGrid(size);
@@ -48,6 +53,7 @@ gridSizeBtn.addEventListener('click', function(){
     gridElements = createGrid(size);
 });
 
+//when clear button pressed, remove grid and create new grid w/ same size, same array
 const clearBtn = document.querySelector('#clear')
 clearBtn.addEventListener('click', function(){
     removeGrid(size);
